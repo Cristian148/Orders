6308fe8fdeb6226469aa5814d4bae13feb5edfda
Cristian <caal25@hotmail.com>
2024-05-16 18:33:33 -0300

    basic project creation

--- Orders/Orders.Backend/Controllers/WeatherForecastController.cs
+++ Orders/Orders.Backend/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace Orders.Backend.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+                private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
