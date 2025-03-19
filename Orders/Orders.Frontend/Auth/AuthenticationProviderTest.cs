ec4a1b906325d0830585ed064a8a1c91f6fdbb46
Cristian <abbatecristian30@gmail.com>
2025-03-19 08:34:18 -0300

    Seguridad Hardcodeada en Frontend

---
+++ Orders/Orders.Frontend/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Orders.Frontend.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+
+            //var zuluUser = new ClaimsIdentity(authenticationType: "test");
+            var zuluUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Juan"),
+                new Claim("LastName", "Zulu"),
+                new Claim(ClaimTypes.Name, "merkabak148@gmail.com"),
+                new Claim(ClaimTypes.Role, "User")
+            },
+            authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+
+}
